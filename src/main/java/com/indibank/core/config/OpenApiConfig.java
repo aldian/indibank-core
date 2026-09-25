4197f1d541522754371f0b1f49cbaf84327786bf
Aldian Fazrihady <aldian@axiobionics.com>
2026-09-25 08:35:54 +0700

    fix(docs): remove redundant author and maintainer sections

--- src/main/java/com/indibank/core/config/OpenApiConfig.java
+++ src/main/java/com/indibank/core/config/OpenApiConfig.java
@@ -1,7 +1,6 @@
 package com.indibank.core.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
@@ -21,8 +20,6 @@ public class OpenApiConfig {
                         .version("1.0.0")
                         .description("High-Throughput Core Banking Interbank Fund Transfer & Event-Driven Ledger System.\n" +
                                 "Showcases Java 21, Apache Kafka, Redis, and Oracle DB.")
-                        .contact(new Contact()
-                                .name("IndiBank Core Engineering"))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
